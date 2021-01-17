be4ff3793e5d62dae098112eeb11173f11fe5cab
armen <armenmerikyan@gmail.com>
2021-01-17 11:04:54 -0800

    inital version

--- autoconfig.sh
+++ autoconfig.sh
@@ -5,7 +5,7 @@ sudo apt-get install build-essential libtool autotools-dev automake pkg-config l
 sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
 sudo apt-get install libboost-all-dev -y
 sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:bitcoin/bitcoin 
+sudo add-apt-repository ppa:bitcoin/bitcoin -sk
 sudo apt-get update -y
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 sudo apt-get install libminiupnpc-dev -y
