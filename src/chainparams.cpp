ee4e6f5858ba44cecdd5ddf8be54a57a4ea63ef8
armen <armenmerikyan@gmail.com>
2021-01-17 13:35:35 -0800

    Initial build

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -101,11 +101,16 @@ public:
          * The message start string is designed to be unlikely to occur in normal data.
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
+         pchMessageStart[0] = 0xfd;
+         pchMessageStart[1] = 0xc8;
+         pchMessageStart[2] = 0xb0;
+         pchMessageStart[3] = 0xdl;
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
+
+        pchMessageStart[0] = 0xfd;
+        pchMessageStart[1] = 0xc8;
+        pchMessageStart[2] = 0xb0;
+        pchMessageStart[3] = 0xdl;
         nDefaultPort = 1980;
         nPruneAfterHeight = 100000;
         m_assumed_blockchain_size = 22;
